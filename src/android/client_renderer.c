49bb3b543f22b2eb1141fa9acff78f22d44e5a77
Xtr126 <k.gunetilleke@gmail.com>
2026-01-04 23:02:57 +0530

    refactor: cleanup imports in client_renderer

--- src/android/client_renderer.c
+++ src/android/client_renderer.c
@@ -1,5 +1,4 @@
-#include <wlr/render/wlr_renderer.h>
-#include <wlr/types/wlr_buffer.h>
+#include "client_renderer.h"
 
 bool render_client_buffer_to_buffer(struct wlr_renderer *renderer,
         struct wlr_buffer *src_buffer, struct wlr_buffer *dst_buffer) {
--- src/android/client_renderer.h
+++ src/android/client_renderer.h
@@ -1,2 +1,5 @@
+#include <wlr/render/wlr_renderer.h>
+#include <wlr/types/wlr_buffer.h>
+
 bool render_client_buffer_to_buffer(struct wlr_renderer *renderer,
         struct wlr_buffer *src_buffer, struct wlr_buffer *dst_buffer);
